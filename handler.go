bcf70e92a01ce8b92b19790dc6f23a4fe4a040a6
Gusyatnikova <crysnat08@gmail.com>
2021-08-19 14:43:16 +0500

    Added hardcoded YAML redirections

--- handler.go
+++ handler.go
@@ -1,16 +1,49 @@
 package urlshort
 
 import (
+	"gopkg.in/yaml.v2"
 	"net/http"
 )
 
-func MapHandler(inToRedirected map[string]string, fallback http.Handler) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+type redirectItem struct {
+	Path string `yaml:"path"`
+	URL string `yaml:"url"`
+}
+
+func MapHandler(inToRedirected map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		val, ok := inToRedirected[r.URL.String()]
 		if ok {
 			http.Redirect(w, r, val, http.StatusMovedPermanently)
 		} else {
 			fallback.ServeHTTP(w, r)
 		}
-	})
+	}
+}
+
+func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYAML(yaml)
+	if err != nil {
+		return fallback.ServeHTTP, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(yamlBytes []byte) ([]redirectItem, error) {
+	var yamlList []redirectItem
+	err := yaml.Unmarshal(yamlBytes[1:len(yamlBytes) - 1], &yamlList)
+	if err != nil {
+		return nil, err
+	}
+	return yamlList, nil
+}
+
+func buildMap(yamlList []redirectItem) map[string]string {
+	var yamlMap = map[string]string{}
+	var entry redirectItem
+		for _, entry = range yamlList {
+			yamlMap[entry.Path] = entry.URL
+		}
+	return yamlMap
 }
--- main/main.go
+++ main/main.go
@@ -8,15 +8,26 @@ import (
 
 func main() {
 	http.HandleFunc("/", home)
-	gotToRedirect := map[string]string {
+	gotToRedirect := map[string]string{
 		"/google": "https://google.com/",
-		"/game": "https://tetris.com/play-tetris/",
+		"/game":   "https://tetris.com/play-tetris/",
 		"/github": "https://github.com/Gusyatnikova/",
 	}
 
-	http.ListenAndServe(
-		":3000",
-		urlshort.MapHandler(gotToRedirect, http.DefaultServeMux))
+	mapHandler := urlshort.MapHandler(gotToRedirect, http.DefaultServeMux)
+	yaml := `
+- path: /Fun/TV-series
+  url: https://disneyplusoriginals.disney.com/show/the-mandalorian
+- path: /Fun/The-Child
+  url: https://static.wikia.nocookie.net/starwars/images/4/43/TheChild-Fathead.png/revision/latest/scale-to-width-down/500?cb=20201031231040
+- path: /Fun/Grogu
+  url: https://static.wikia.nocookie.net/starwars/images/4/43/TheChild-Fathead.png/revision/latest/scale-to-width-down/500?cb=20201031231040
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	http.ListenAndServe(":3000", http.Handler(yamlHandler))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
