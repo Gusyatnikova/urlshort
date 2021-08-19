a0bc2785e2f27eb36d56eac5c3c99ae80ffd2fd0
Gusyatnikova <crysnat08@gmail.com>
2021-08-19 12:31:05 +0500

    Implementation of redirection

--- main/main.go
+++ main/main.go
@@ -7,37 +7,19 @@ import (
 )
 
 func main() {
-	mux := defaultMux()
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/redirect": "https://www.google.com/",
+	http.HandleFunc("/", home)
+	gotToRedirect := map[string]string {
+		"/google": "https://google.com/",
+		"/game": "https://tetris.com/play-tetris/",
+		"/github": "https://github.com/Gusyatnikova/",
 	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
-	http.ListenAndServe(":3000", mapHandler)
-/*
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)*/
+
+	http.ListenAndServe(
+		":3000",
+		urlshort.MapHandler(gotToRedirect, http.DefaultServeMux))
 }
 
-func defaultMux() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-	return mux
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "<h3>I'm a default serveMux</h3>")
 }
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
-}
-
